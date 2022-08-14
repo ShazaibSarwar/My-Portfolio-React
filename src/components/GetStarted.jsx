d9a924de13a71dcdaebfbad9cd8994a2ff3a4d02
Shazaib Sarwar <shazaibs355@gmail.com>
2022-08-15 01:34:02 +0500

    new

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -2,7 +2,7 @@ import styles from "../style";
 import { arrowUp } from "../assets";
 
 const GetStarted = () => (
-  <a href="https://drive.google.com/file/d/1A6w-LHo-_l6EzUYL08FzOSMzhCRrQskB/view?usp=sharing">
+  <a href="https://drive.google.com/file/d/1A6w-LHo-_l6EzUYL08FzOSMzhCRrQskB/view?usp=sharing" target="_blank">
     <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[4px] cursor-pointer`}>
       <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
         <div className={`${styles.flexStart} flex-row`}>
